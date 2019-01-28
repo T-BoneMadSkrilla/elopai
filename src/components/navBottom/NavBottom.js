8cdeb0887c97091738ac2bf3a15b0f9caca79261
Tyler Eftink <42481212+T-BoneMadSkrilla@users.noreply.github.com>
2019-01-28 12:21:04 -0600

    took away request for demo in bottom nav

--- src/components/navBottom/NavBottom.js
+++ src/components/navBottom/NavBottom.js
@@ -9,10 +9,9 @@ class NavBottom extends Component {
                 <div className="navBottomWhiteDivider"></div>
                 <div className="navBottomNavyDivider"></div>
             <div className="navBottomNavy">
-                <Link to="/" className="bottomNavLinkStyle">Home</Link>
+                <Link to="/info" className="bottomNavLinkStyle">Home</Link>
                 <Link to='/solutions'className="bottomNavLinkStyle">Solutions</Link>
                 <Link to='/about' className="bottomNavLinkStyle">Company</Link>
-                <Link to='/requestfordemo'className="bottomNavLinkStyle">Request For Demo</Link>
             </div>
             </div>
         )
